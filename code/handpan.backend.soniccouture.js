8a6186c19647482e4e75e887341504b804d473e7
Edsko de Vries <edsko@well-typed.com>
2021-01-02 12:16:51 +0100

    Skeleton backend

---
+++ code/handpan.backend.soniccouture.js
@@ -0,0 +1,53 @@
+/**
+ * Max Handpan
+ *
+ * @module handpan.backend.soniccouture
+ * @description Background for Soniccouture Pan Drums
+ * @author Edsko de Vries <edsko@edsko.net>
+ * @copyright Edsko de Vries, 2020-2021
+ * @see {@link https://github.com/edsko/max-handpan}
+ * @license BSD-3-Clause
+ */
+
+/*******************************************************************************
+  Device initialization
+*******************************************************************************/
+
+inlets    = 3;
+outlets   = 2;
+autowatch = 0;
+
+/*******************************************************************************
+  Handle M4L messages
+*******************************************************************************/
+
+/**
+ * Initialize device
+ */
+function init() {
+  post("init\n");
+}
+
+/**
+ * Delete observers
+ */
+function deleteObservers() {
+  post("deleteObservers\n");
+}
+
+/**
+ * Handle incoming MIDI messages
+ */
+function list() {
+  if(arguments.length != 2) {
+    error("list: message not understood\n");
+    return;
+  }
+
+  var pitch    = arguments[0];
+  var velocity = arguments[1];
+
+  post("list", pitch, velocity, "\n");
+
+  outlet(0, [pitch, velocity]);
+}
