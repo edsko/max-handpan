f21fb786408235f5424286f8fac91bd67d96f1ea
Edsko de Vries <edsko@well-typed.com>
2021-02-27 09:05:45 +0100

    Set up a live.observer for the button matrix

--- ps/src/Frontend.purs
+++ ps/src/Frontend.purs
@@ -7,6 +7,7 @@ import Effect (Effect)
 import MaxForLive.Global (
     setInlets
   , setOutlets
+  , outlet
   , postLn
   )
 import MaxForLive.Handlers (
@@ -18,7 +19,7 @@ import MaxForLive.Push as Push
 main :: Effect Unit
 main = do
     setInlets  1
-    setOutlets 1
+    setOutlets 2
 
     mPush <- Push.new
     case mPush of
@@ -32,4 +33,5 @@ registerHandlers :: Push -> Effect Unit
 registerHandlers (Push push) = do
     setHandler { inlet: 0, msg: "grab", handler: push.grabButtonMatrix }
     setHandler { inlet: 0, msg: "release", handler: push.releaseButtonMatrix }
-    push.setButtonMatrixColor { col: 1, row: 2 } 10 
+    push.setButtonMatrixColor { col: 1, row: 2 } 10
+    outlet 1 push.buttonMatrixId
